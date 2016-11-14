905a0ec129fbe53f69db1596b39e68087a911477
kvzaytsev <kirill.v.zaytsev@gmail.com>
2016-11-14 23:30:00 +0300

    Map and Popup closing added

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 var i, l,
+    escKey = 27,
     gotLostLink = document.querySelector('.link-lost'),
     gotLostPopup = document.querySelector('.lost-popup'),
     mapLink = document.querySelector('.link-map'),
     mapPopup = document.querySelector('.map-popup'),
     addToCartLinks = document.querySelectorAll('.good-action-buy'),
     addToCartPopup = document.querySelector('.cart-popup'),
-    closePopupBtns = document.querySelectorAll('.popup-close');
+    closePopupBtns = document.querySelectorAll('.popup-close'),
+    allPopups = document.querySelectorAll('.popup');
 
 !!gotLostLink && gotLostLink.addEventListener('click', onGotLostLinkClick, false);
 !!mapLink && mapLink.addEventListener('click', onMapLinkClick, false);
@@ -17,6 +19,14 @@ for (i = 0, l = closePopupBtns.length; i < l; i++) {
 for (i = 0, l = addToCartLinks.length; i < l; i++) {
   addToCartLinks[i].addEventListener('click', onAddToCartLinkClick, false);
 }
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === escKey) {
+    for (i = 0, l = allPopups.length; i < l; i++) {
+      doClosePopup(allPopups[i]);
+    }
+  }
+});
 
 function onAddToCartLinkClick(evt) {
   evt.preventDefault();
@@ -37,9 +47,13 @@ function onClosePopupBtnClick(evt) {
   var target = evt.target,
       popup = getParentByClassName(target, 'popup');
 
-  if (popup) {
+      doClosePopup(popup);
+}
+
+function doClosePopup(popup) {
+    !!popup &&
+    popup.classList.contains("popup-show") &&
     popup.classList.remove('popup-show');
-  }
 }
 
 function getParentByClassName(node, className) {
