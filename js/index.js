7bef5cb932c0153b12d63385a40a36772a298161
kvzaytsev <kirill.v.zaytsev@gmail.com>
2016-11-15 19:14:05 +0300

    gallery

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
+var KEYS = {
+  ESC: 27
+}
+
 var i, l,
-    escKey = 27,
     gotLostLink = document.querySelector('.link-lost'),
     gotLostPopup = document.querySelector('.lost-popup'),
     mapLink = document.querySelector('.link-map'),
@@ -12,6 +15,75 @@ var i, l,
 !!gotLostLink && gotLostLink.addEventListener('click', onGotLostLinkClick, false);
 !!mapLink && mapLink.addEventListener('click', onMapLinkClick, false);
 
+initServicesTabs();
+initGallery();
+
+function initServicesTabs() {
+  var tabs = document.querySelectorAll('.services-list-item'),
+      services = document.querySelector('.services-content-right').children;
+
+  for (i = 0, l = tabs.length; i < l; i++) {
+    tabs[i].addEventListener('click', onServicesTabClick, false);
+  }
+
+  function onServicesTabClick(evt) {
+    evt.preventDefault();
+
+    var target = evt.currentTarget,
+        selector = evt.target.getAttribute('href');
+
+    for (i = 0, l = services.length; i < l; i++) {
+      services[i].classList.remove('active');
+    }
+
+    for (i = 0, l = tabs.length; i < l; i++) {
+      tabs[i].classList.remove('active');
+    }
+
+    target.classList.add('active');
+    document.querySelector(selector).classList.add('active');
+  }
+
+}
+
+function initGallery() {
+    if (document.querySelector('.gallery') === null) {
+      return;
+    }
+
+    var
+      galleryLeftBtn = document.querySelector('.gallery-btn-left'),
+      galleryRightBtn = document.querySelector('.gallery-btn-right'),
+      gallarySlidedBlock = document.querySelector('.slides-block');
+
+    galleryLeftBtn.addEventListener('click', onGalleryLeftBtnClick, false);
+    galleryRightBtn.addEventListener('click', onGalleryRightBtnClick, false);
+
+    function onGalleryLeftBtnClick(evt) {
+      goToNext('prev');
+    }
+
+    function onGalleryRightBtnClick(evt) {
+      goToNext('next');
+    }
+
+    function goToNext(direction) {
+      var current = document.querySelector('.slides-block .active'),
+        next = direction === 'prev'
+          ? current.previousElementSibling
+          : current.nextElementSibling
+
+      next = !!next
+          ? next
+          : direction === 'prev'
+            ? gallarySlidedBlock.lastElementChild
+            : gallarySlidedBlock.firstElementChild;
+
+      current.classList.remove('active');
+      next.classList.add('active');
+    }
+}
+
 for (i = 0, l = closePopupBtns.length; i < l; i++) {
   closePopupBtns[i].addEventListener('click', onClosePopupBtnClick, false);
 }
@@ -21,7 +93,7 @@ for (i = 0, l = addToCartLinks.length; i < l; i++) {
 }
 
 window.addEventListener("keydown", function(event) {
-  if (event.keyCode === escKey) {
+  if (event.keyCode === KEYS.ESC) {
     for (i = 0, l = allPopups.length; i < l; i++) {
       doClosePopup(allPopups[i]);
     }
@@ -44,10 +116,9 @@ function onGotLostLinkClick(evt) {
 }
 
 function onClosePopupBtnClick(evt) {
-  var target = evt.target,
-      popup = getParentByClassName(target, 'popup');
+  var target = evt.target;
 
-      doClosePopup(popup);
+  doClosePopup( getParentByClassName(target, 'popup'));
 }
 
 function doClosePopup(popup) {
