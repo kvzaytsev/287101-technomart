fa13b1760ac56251f5f5aac7e38bb0b488531c95
kvzaytsev <kirill.v.zaytsev@gmail.com>
2016-11-19 18:03:18 +0300

    Fixes according to remarks

--- js/index.js
+++ js/index.js
@@ -5,21 +5,21 @@ var KEYS = {
 var i, l,
     savedName = localStorage.getItem("name"),
     savedEmail = localStorage.getItem("email"),
-    gotLostLink = document.querySelector('.link-lost'),
-    gotLostPopup = document.querySelector('.lost-popup'),
-    mapLink = document.querySelector('.link-map'),
-    mapPopup = document.querySelector('.map-popup'),
-    addToCartLinks = document.querySelectorAll('.good-action-buy'),
-    addToCartPopup = document.querySelector('.cart-popup'),
-    closePopupBtns = document.querySelectorAll('.popup-close'),
-    allPopups = document.querySelectorAll('.popup'),
-    nameInput = document.querySelector('#form-name'),
-    emailInput = document.querySelector('#form-email'),
-    textArea = document.querySelector('#form-text'),
-    form = document.querySelector('.lost-popup form');
+    gotLostLink = document.querySelector(".link-lost"),
+    gotLostPopup = document.querySelector(".lost-popup"),
+    mapLink = document.querySelector(".link-map"),
+    mapPopup = document.querySelector(".map-popup"),
+    addToCartLinks = document.querySelectorAll(".good-action-buy"),
+    addToCartPopup = document.querySelector(".cart-popup"),
+    closePopupBtns = document.querySelectorAll(".popup-close"),
+    allPopups = document.querySelectorAll(".popup"),
+    nameInput = document.querySelector("#form-name"),
+    emailInput = document.querySelector("#form-email"),
+    textArea = document.querySelector("#form-text"),
+    form = document.querySelector(".lost-popup form");
 
-!!gotLostLink && gotLostLink.addEventListener('click', onGotLostLinkClick, false);
-!!mapLink && mapLink.addEventListener('click', onMapLinkClick, false);
+!!gotLostLink && gotLostLink.addEventListener("click", onGotLostLinkClick, false);
+!!mapLink && mapLink.addEventListener("click", onMapLinkClick, false);
 
 !!form && form.addEventListener("submit", function(event) {
     if (!(nameInput.value && emailInput.value)) {
@@ -37,82 +37,82 @@ initServicesTabs();
 initGallery();
 
 function initServicesTabs() {
-  var tabs = document.querySelectorAll('.services-list-item');
+  var tabs = document.querySelectorAll(".services-list-item");
 
   if (!tabs.length) {
     return;
   }
 
-  var services = document.querySelector('.services-content-right').children;
+  var services = document.querySelector(".services-content-right").children;
 
   for (i = 0, l = tabs.length; i < l; i++) {
-    tabs[i].addEventListener('click', onServicesTabClick, false);
+    tabs[i].addEventListener("click", onServicesTabClick, false);
   }
 
   function onServicesTabClick(evt) {
     evt.preventDefault();
 
     var target = evt.currentTarget,
-        selector = evt.target.getAttribute('href');
+        selector = evt.target.getAttribute("href");
 
     for (i = 0, l = services.length; i < l; i++) {
-      services[i].classList.remove('active');
+      services[i].classList.remove("active");
     }
 
     for (i = 0, l = tabs.length; i < l; i++) {
-      tabs[i].classList.remove('active');
+      tabs[i].classList.remove("active");
     }
 
-    target.classList.add('active');
-    document.querySelector(selector).classList.add('active');
+    target.classList.add("active");
+    document.querySelector(selector).classList.add("active");
   }
 
 }
 
 function initGallery() {
-    if (document.querySelector('.gallery') === null) {
+    if (document.querySelector(".gallery") === null) {
       return;
     }
 
     var
-      galleryLeftBtn = document.querySelector('.gallery-btn-left'),
-      galleryRightBtn = document.querySelector('.gallery-btn-right'),
-      gallarySlidedBlock = document.querySelector('.slides-block');
+      galleryLeftBtn = document.querySelector(".gallery-btn-left"),
+      galleryRightBtn = document.querySelector(".gallery-btn-right"),
+      gallarySlidedBlock = document.querySelector(".slides-block");
 
-    galleryLeftBtn.addEventListener('click', onGalleryLeftBtnClick, false);
-    galleryRightBtn.addEventListener('click', onGalleryRightBtnClick, false);
+    galleryLeftBtn.addEventListener("click", onGalleryLeftBtnClick, false);
+    galleryRightBtn.addEventListener("click", onGalleryRightBtnClick, false);
 
     function onGalleryLeftBtnClick(evt) {
-      goToNext('prev');
+      goToNext("prev");
     }
 
     function onGalleryRightBtnClick(evt) {
-      goToNext('next');
+      goToNext("next");
     }
 
     function goToNext(direction) {
-      var current = document.querySelector('.slides-block .active'),
-        next = direction === 'prev'
+      var current = document.querySelector(".slides-block .active"),
+        next = direction === "prev"
           ? current.previousElementSibling
           : current.nextElementSibling
 
       next = !!next
           ? next
-          : direction === 'prev'
+          : direction === "prev"
             ? gallarySlidedBlock.lastElementChild
             : gallarySlidedBlock.firstElementChild;
 
-      current.classList.remove('active');
-      next.classList.add('active');
+      current.classList.remove("active");
+      next.classList.add("active");
     }
 }
 
 for (i = 0, l = closePopupBtns.length; i < l; i++) {
-  closePopupBtns[i].addEventListener('click', onClosePopupBtnClick, false);
+  closePopupBtns[i].addEventListener("click", onClosePopupBtnClick, false);
 }
 
 for (i = 0, l = addToCartLinks.length; i < l; i++) {
-  addToCartLinks[i].addEventListener('click', onAddToCartLinkClick, false);
+  addToCartLinks[i].addEventListener("click", onAddToCartLinkClick, false);
 }
 
 window.addEventListener("keydown", function(event) {
@@ -125,17 +125,17 @@ window.addEventListener("keydown", function(event) {
 
 function onAddToCartLinkClick(evt) {
   evt.preventDefault();
-  addToCartPopup.classList.add('popup-show');
+  addToCartPopup.classList.add("popup-show");
 }
 
 function onMapLinkClick(evt) {
   evt.preventDefault();
-  mapPopup.classList.add('popup-show');
+  mapPopup.classList.add("popup-show");
 }
 
 function onGotLostLinkClick(evt) {
   evt.preventDefault();
-  gotLostPopup.classList.add('popup-show');
+  gotLostPopup.classList.add("popup-show");
   if (!!savedName && !!savedEmail) {
     nameInput.value = savedName;
     emailInput.value = savedEmail;
@@ -148,13 +148,13 @@ function onGotLostLinkClick(evt) {
 function onClosePopupBtnClick(evt) {
   var target = evt.target;
 
-  doClosePopup( getParentByClassName(target, 'popup'));
+  doClosePopup( getParentByClassName(target, "popup"));
 }
 
 function doClosePopup(popup) {
     !!popup &&
     popup.classList.contains("popup-show") &&
-    popup.classList.remove('popup-show');
+    popup.classList.remove("popup-show");
 }
 
 function getParentByClassName(node, className) {
